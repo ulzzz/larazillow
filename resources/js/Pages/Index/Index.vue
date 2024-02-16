83e8a6ae3e9e9ec5550ff957378f5b0cf2ce4d70
Luis <emilioluistarnate1@gmail.com>
2024-02-17 01:09:11 +0800

    edited some files

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+        <div>index</div>
+    <div>
+        The message is {{ message }}
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps({
+    message: String
+})
+
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<template>
+          <div>Show</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
